8cadaa9f278a784e23bd5b601b00db7d71586f6a
rryanhan <141535726+rryanhan@users.noreply.github.com>
2026-08-14 00:31:33 -0700

    Add: task-scheduling - submission-4

---
+++ Data Structures & Algorithms/task-scheduling/submission-4.py
@@ -0,0 +1,34 @@
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        taskCount = Counter(tasks)
+
+        heap = [-cnt for cnt in taskCount.values()]
+        heapq.heapify(heap)
+
+        q = deque()
+        time = 0
+
+        while heap or q:
+            if q:
+                if q[0][1] == time:
+                    tasktoadd, availabletime = q.popleft()
+                    heapq.heappush(heap, tasktoadd)
+            if heap:
+                task = heapq.heappop(heap)
+                task += 1
+                if task != 0:
+                    q.append([task, time + n + 1])
+            
+            
+            time += 1
+        return time
+
+
+
+
+        
+
+
+# a max heap to keep track of tasks we need to process, sorted by most
+
+# a queue that keeps track of tasks in cooldown and when they are next available
