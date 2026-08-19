f14ed8acd8b41fd0c80448d2adceaabcd52238a2
rryanhan <141535726+rryanhan@users.noreply.github.com>
2026-08-19 14:43:26 -0700

    Add: my-calendar-i - submission-4

---
+++ Data Structures & Algorithms/my-calendar-i/submission-4.py
@@ -0,0 +1,21 @@
+class MyCalendar:
+    
+    def __init__(self):
+        self.bookings = []
+        
+
+    def book(self, startTime: int, endTime: int) -> bool:
+        for start, end in self.bookings:
+            # if there is overlap, False, don't insert
+            if end > startTime and endTime > start:
+                return False
+        self.bookings.append([startTime, endTime])
+        return True
+        
+
+
+# Your MyCalendar object will be instantiated and called as such:
+# obj = MyCalendar()
+# param_1 = obj.book(startTime,endTime)
+#   <--------->
+# <---->.        <------>
