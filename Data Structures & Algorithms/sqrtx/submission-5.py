a759229aeddd17d1c0c50dd0eded6a3908179bbc
rryanhan <141535726+rryanhan@users.noreply.github.com>
2026-08-20 00:01:06 -0700

    Add: sqrtx - submission-5

---
+++ Data Structures & Algorithms/sqrtx/submission-5.py
@@ -0,0 +1,24 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        
+        l, r = 0, x
+        res = -9999
+
+        while l <= r:
+            mid = (l + r) // 2
+            print(mid)
+            if mid * mid == x:
+                return mid
+            if mid * mid > x:
+                # need to search lower
+                r = mid - 1
+            else:
+                # put as valid, look for larger numbers
+                res = max(res, mid)
+                l = mid + 1
+        return res
+
+# 1  2  3  4  5  6  7  8  9
+#             m 
+
+        
